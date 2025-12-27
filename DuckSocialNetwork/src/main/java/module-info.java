f549c3758c36742c9aec2c6757c7ec6308a0c36d
lupse <lupse.ioan.victor@gmail.com>
2025-12-27 22:26:17 +0200

    opened domain classes to jackson serializer to allow deserialization

--- DuckSocialNetwork/src/main/java/module-info.java
+++ DuckSocialNetwork/src/main/java/module-info.java
@@ -6,7 +6,9 @@ module org.example {
     requires javafx.graphics;
     requires java.desktop;
     requires org.postgresql.jdbc;
-    requires org.example;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
 
     exports org.example;
     exports org.ui.gui;
@@ -16,4 +18,9 @@ module org.example {
 
     opens org.ui.gui to javafx.fxml;
     opens org.domain.dtos.guiDTOS to javafx.base;
+    opens org.domain to com.fasterxml.jackson.databind;
+    opens org.domain.users.relationships.notifications to com.fasterxml.jackson.databind;
+    opens org.domain.users to com.fasterxml.jackson.databind;
+    opens org.domain.users.relationships to com.fasterxml.jackson.databind;
+    opens org.domain.events to com.fasterxml.jackson.databind;
 }
