1f0b9ee6fd10733daf7820ac5338713f4b7cca5a
lupse <lupse.ioan.victor@gmail.com>
2025-12-28 08:40:48 +0200

    new message data class

---
+++ DuckSocialNetwork/src/main/java/org/domain/users/relationships/notifications/MessageData.java
@@ -0,0 +1,21 @@
+package org.domain.users.relationships.notifications;
+
+import org.domain.users.relationships.messages.Message;
+import org.utils.enums.actions.MessageAction;
+
+public class MessageData {
+    private final Message message;
+    private final MessageAction action;
+
+    public MessageData(Message message, MessageAction action) {
+        this.message = message;
+        this.action = action;
+    }
+
+    public MessageAction getAction() {
+        return action;
+    }
+    public Message getMessage() {
+        return message;
+    }
+}
